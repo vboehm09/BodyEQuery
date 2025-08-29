ad9087ec30d3d38d36b693437fdc5f7a87a80a96
Victor Boehm <victor.b.assumpcao@aluno.senai.br>
2025-08-29 15:40:56 -0300

    feat: Adiciona rota bruxos com filtro

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import dados from "./src/data/dados.js";
+const { bruxos } = dados
+
 // Criar aplicação com Express e configurar para aceitar JSON
 const app = express();
 app.use(express.json());
@@ -16,7 +19,32 @@ app.get("/", (req, res) => {
 });
 
 
-// Aqui vão todas suas Rotas
+// Query Parameters no Node.js - API de Hogwarts
+app.get('/bruxos', (req, res) => {
+    const { casa, ano, especialidade, nome } = req.query;
+    let resultado = bruxos;
+  
+    if (casa) {
+      resultado = resultado.filter(b => b.casa.toLowerCase() === casa.toLowerCase());
+    }
+  
+    if (ano) {
+      resultado = resultado.filter(b => b.ano == ano);
+    }
+  
+    if (especialidade) {
+      resultado = resultado.filter(b => b.especialidade.toLowerCase().includes(especialidade.toLowerCase()));
+    }
+  
+    if (nome) {
+      resultado = resultado.filter(b => b.nome.toLowerCase().includes(nome.toLowerCase()));
+    }
+  
+    res.status(200).json({
+      total: resultado.length,
+      data: resultado
+    });
+});
 
 
 // Iniciar servidor escutando na porta definida
